ddff80c75164e138d2e3ffd51271cd1494df17e6
Peter Skopal <peter.skopal@hotmail.com>
2018-03-15 21:51:02 +0100

    feat: Bottom Bar and Top Bar added.

--- App.js
+++ App.js
@@ -1,20 +1,85 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
+import Tabbar from 'react-native-tabbar-bottom';
+
+const MyStatusBar = ({backgroundColor, ...props}) => (
+  <View style={[styles.statusBar, { backgroundColor }]}>
+    <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+);
 
 export default class App extends React.Component {
+
+  constructor() {
+    super()
+    this.state = {
+      navigation: {
+        page: "HomeScreen",
+      },
+      split: null
+    }
+  }
+
+  renderHomeScreen() {
+    return <Text>Dashboard</Text>
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        
+        <MyStatusBar backgroundColor="#0095ff" barStyle="light-content" />
+        <View style={styles.appBar}>
+          <Text style={styles.appBarTitle}>MONEY SPLITTER</Text>
+        </View>
+        <View style={styles.content}>
+          {this.state.navigation.page === "HomeScreen" && this.renderHomeScreen()}
+          {this.state.navigation.page === "SearchScreen" && <Text>Screen5</Text>}
+        </View>
+        <Tabbar
+          tabbarBgColor={'#333'}
+          iconColor={'#ccc'}
+          selectedIconColor={'#0095ff'}
+          stateFunc={(tab) => {
+            this.setState({navigation: { page: tab.page }})
+          }}
+          activePage={this.state.navigation.page}
+          tabs={[
+            {
+              page: "HomeScreen",
+              icon: "home",
+            },
+            {
+              page: "SearchScreen",
+              icon: "search",
+            },
+          ]}
+        />
       </View>
     );
   }
 }
 
+const STATUSBAR_HEIGHT = Platform.OS === 'ios' ? 20 : StatusBar.currentHeight;
+const APPBAR_HEIGHT = Platform.OS === 'ios' ? 44 : 56;
+
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+  },
+  statusBar: {
+    height: STATUSBAR_HEIGHT,
+  },
+  appBar: {
+    backgroundColor:'#0095ff',
+    height: APPBAR_HEIGHT,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  appBarTitle: {
+    color: '#fff',
+  },
+  content: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
